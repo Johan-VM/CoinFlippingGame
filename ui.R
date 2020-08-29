42b2eab27845416fa3ae17111b84bdef3923acca
Johan Vásquez Mazo <64623535+Johan-VM@users.noreply.github.com>
2020-08-29 15:30:12 -0500

    Added info tab.

--- ui.R
+++ ui.R
@@ -14,7 +14,7 @@ shinyUI(fluidPage(
     
     sidebarLayout(
         sidebarPanel(
-            numericInput(inputId = "p", label = h4(HTML("Enter the true <b>probability</b> of getting <b>Heads</b>:")), 
+            numericInput(inputId = "p", label = h4(HTML("Enter the true <b>probability</b> of getting <b>Heads</b>:")),
                 value = 0.5, min = 0, max = 1),
             
             actionButton("flip", label = strong("Flip the coin!"), style = "color:#117985"),
@@ -25,19 +25,39 @@ shinyUI(fluidPage(
             sliderInput(inputId = "headsBet", label = h4(HTML("Select your <b>bet</b> on <b>Heads</b> [$]:")), 
                 value = 5, min = 0, max = 10, step = 0.5),
             
-            conditionalPanel(condition = "input.betting == 2", 
-                sliderInput(inputId = "tailsBet", label = h4(HTML("Select your opponent's <b>bet</b> on <b>Tails</b> [$]:")), 
-                    value = 5, min = 0, max = 10, step = 0.5))
+            conditionalPanel(condition = "input.betting == 2",
+                sliderInput(inputId = "tailsBet", value = 5, min = 0, max = 10, step = 0.5,
+                    label = h4(HTML("Select your opponent's <b>bet</b> on <b>Tails</b> [$]:"))))
         ),
         
         mainPanel(
-            h3(strong(textOutput("result")), style = "color:#0f0947"),
-            
-            h3(strong(htmlOutput("totalEarned"))),
-            
-            plotOutput("drawPlot"),
-            
-            plotOutput("totalsPlot")
+            tabsetPanel(id = "Tabs",
+                tabPanel(
+                    title = strong("Results", style = "color:#117985"),
+                    
+                    h3(strong(textOutput("result")), style = "color:#0e1236"),
+                    
+                    h3(strong(htmlOutput("totalEarned"))),
+                    
+                    plotOutput("drawPlot"),
+                    
+                    plotOutput("totalsPlot")
+                ),
+                
+                tabPanel(
+                    title = strong("About", style = "color:black"),
+                    
+                    h4(strong("This app simulates a coin flipping betting game. You bet on heads and your opponent bets 
+                    on tails. You can modify the coin's true probability of getting heads, the amount of money you bet, 
+                    and whether the game is fair or potentially unfair."), style = "color:black"),
+                    
+                    h4(strong("Keep in mind that entering an extreme probability (i.e., 0 for always tails and 1 for 
+                    always heads) will make the game always unfair, so there is no point in betting for those two cases."), 
+                        style = "color:black"),
+                    
+                    h4(strong(HTML("Developed by Johan V&aacute;squez Mazo. Powered by RStudio Shiny.")), 
+                        style = "color:#0f0947"))
+            )
         )
-    )    
+    )
 ))
